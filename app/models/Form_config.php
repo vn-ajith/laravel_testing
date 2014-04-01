bb4273b1edb1363bc3e3e3f708f4740f07057e26
Ajith <ajith.p@outlook.com>
2014-04-01 11:23:39 +0530

    added table creation for form configuration

--- app/models/Form_config.php
+++ app/models/Form_config.php
@@ -9,5 +9,32 @@ class Form_config extends Eloquent
 {
 	protected $table = 'form_configs';
 	public static $key = 'form_id';
+
+	/*
+		Table creation is done in constructor.
+		Table is created if not exits
+	*/	
+	public function __construct()
+	{
+		if (!Schema::hasTable('form_configs'))
+		{
+    			Schema::create('form_configs',function($table){
+				$table->increments('form_id'); // Form id
+				$table->string('form_name','50'); // Form name
+				$table->text('form_desc')->nullable();  // Description of form
+									// Form description can be null
+				$table->text('form_url');  // URL to which form is posted
+				$table->integer('num_fields');  // Number of fields in form
+			
+			});			
+		}
+		
+	}
+	
+	public function col_exists($col_name)
+	{
+		
+		
+	}
 }
 ?>
