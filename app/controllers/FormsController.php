88c50d683aa8cdbe33ab0a509d0fcebb1d299cc2
Ajith <ajith.p@outlook.com>
2014-04-01 12:41:04 +0530

    added function for creating new column for field in form

--- app/controllers/FormsController.php
+++ app/controllers/FormsController.php
@@ -15,6 +15,18 @@ class FormsController extends BaseController
 	/*
 		Function for saving form parameter and form data to databse
 		This function can handle multiple types of forms
+		
+		Naming that should be followed for form fields: 
+			single line text : SLT_(number) eg: SLT_1, SLT_20
+			Number : NUM_(number) eg: NUM_23
+			Paragraph text: PARAGH_(number)  eg: PARAGH_25
+			Checkbox: CHECK_(number) eg: CHECK_56
+			Multiple choice: MCHOICE_(number) eg: MCHOICE_3
+			Dropdown : DROPDN_(number) eg: DROPDN_2
+		
+		Numbers should be conitnuous
+		eg:  if 3 single line fields, then names of fields should be SLT_1, SLT_2, SLT_3 
+			
 	*/
 	public function save_form()
 	{
--- app/models/Form_data.php
+++ app/models/Form_data.php
@@ -31,7 +31,8 @@ class Form_data extends Eloquent
 	{
 		if(Schema::hasColumn('form_datas'))
 		{
-			return true;
+			$num = stristr($col_name, '_', true);
+			return $num;
 		}
 		else
 		{
@@ -40,16 +41,73 @@ class Form_data extends Eloquent
 	}
 	/*
 		Function to create a column in tabele dynamically
-		col_name identifies new column to be created 
+		num identifies number attached to the new column name
 		type identifies field type to be created
 		
 		one field in form corresponds to following fields=> fields name and field value
+		type :
+							  DATABASE TYPE
+			1: Single line field   ---------> string
+			2: Number              ---------> integer
+			3: Paragraph text      ---------> text
+			4: Checkbox            ---------> text
+			5: Multiple choice     ---------> text
+			6.: Dropdown           ---------> text
+		
+
+		single line text : SLT_(number) eg: SLT_1, SLT_20
+			Number : NUM_(number) eg: NUM_23
+			Paragraph text: PARAGH_(number)  eg: PARAGH_25
+			Checkbox: CHECK_(number) eg: CHECK_56
+			Multiple choice: MCHOICE_(number) eg: MCHOICE_3
+			Dropdown : DROPDN_(number) eg: DROPDN_2
 	*/
 	
-	public function create_col($col_name,$type)
+	public function create_col($num = 0,$type)
 	{
+			//$type_array = array(1=>'SLT',2=>'NUM',3=>'PARAGH',4=>'CHECK',5=>'MCHOICE',6=>'DROPDN');
+						
+			$col_name_db = 'field_'.($num+1).'_name';		// new column to be generated
+			$value_name_db = 'field_'.($num+1).'_value';  // value field of new column 
+			
+			Schema::table('form_datas', function($table)
+			{
+				
+				$table->string($col_name_db,'100')
+				if($type == 1)
+				{
+					$table->string($value_name_db,'100')->after($col_name_db);
+				}
+				else if($type == 2)
+				{
+					$table->integer($value_name_db,'100')->after($col_name_db);
+				}
+				else
+				{
+					$table->text($value_name_db,'100')->after($col_name_db);
+				}
+				
+				/*
+					column is created in such way that same type fields are adjacent
+				*/
+
+				
+			});
 		
 		
 	}
+
 }
+/*
+eg: 
+Table : form_datas
+--------------------------------------------------------------------
+form_id | field_1_name | field_1_value | ...........................
+--------------------------------------------------------------------
+   1 :  | SLT_1        | this is test  | ............................
+---------------------------------------------------------------------
+   2 :  | NUM_1        | 563           | ............................
+---------------------------------------------------------------------
+
+*/
 ?>
