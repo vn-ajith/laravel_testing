22c918e93219b831a2af12b37a5b035d88215a87
Ajith <ajith.p@outlook.com>
2014-04-24 08:56:42 +0530

    working on controller logic for box arrangement

--- app/views/Forms/arrange_page_elements.blade.php
+++ app/views/Forms/arrange_page_elements.blade.php
@@ -1,28 +1,33 @@
 @extends('../layout')
 @section('content')
-<div>{{$page->id}}</div>
+<!-- <div>{{$page->page_settings}}</div> -->
 <div class="row">
-<div class="col-md-12">.col-md-12</div>
+{{print_r($main_content)}}
+
+<div class="col-md-12">
+{{print_r($form_data)}}
+<div class="col-md-4">.col-md-4</div>
+<div class="col-md-8">.col-md-8</div>
+  <div class="col-md-4">.col-md-4</div>
+<div class="col-md-8">.col-md-8</div>
+  <div class="col-md-4">.col-md-4</div>
+<div class="col-md-8">.col-md-8</div>
+  <div class="col-md-4">.col-md-4</div>
+<div class="col-md-8">.col-md-8</div>
+  <div class="col-md-4">.col-md-4</div>
+<div class="col-md-8">.col-md-8</div>
+  <div class="col-md-4">.col-md-4</div>
+<div class="col-md-8">.col-md-8</div>
+  <div class="col-md-4">.col-md-4</div>
+<div class="col-md-8">.col-md-8</div>
+</div>
 <div class="col-md-12">.col-md-12</div>
 <div class="col-md-12">.col-md-12</div>
 <div class="col-md-12">.col-md-12</div>
 <div class="col-md-12">.col-md-12</div>
 <div class="col-md-12">.col-md-12</div>
 
-  <div class="col-md-4">.col-md-4</div>
-<div class="col-md-8">.col-md-8</div>
-  <div class="col-md-4">.col-md-4</div>
-<div class="col-md-8">.col-md-8</div>
-  <div class="col-md-4">.col-md-4</div>
-<div class="col-md-8">.col-md-8</div>
-  <div class="col-md-4">.col-md-4</div>
-<div class="col-md-8">.col-md-8</div>
-  <div class="col-md-4">.col-md-4</div>
-<div class="col-md-8">.col-md-8</div>
-  <div class="col-md-4">.col-md-4</div>
-<div class="col-md-8">.col-md-8</div>
-  <div class="col-md-4">.col-md-4</div>
-<div class="col-md-8">.col-md-8</div>
+  
 
 
 </div>
